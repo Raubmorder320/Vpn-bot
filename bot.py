ef9943f92dc4eeb69533cfc0d5fabbce0c82c5f5
Anush Student <anush@example.com>
2026-04-05 14:19:48 +0300

    Added new buttons for bot

--- bot.py
+++ bot.py
@@ -15,8 +15,11 @@ load_dotenv()
 bot = Bot(token=os.getenv("BOT_TOKEN"))
 dp = Dispatcher()
 vpn_service = VpnService(os.getenv("CONFIG_PATH"), os.getenv("DB_PATH"))
-builder = InlineKeyboardBuilder()
-builder.row(InlineKeyboardButton(text="Получить VPN 🚀", callback_data="get_config"))
+start_builder = InlineKeyboardBuilder()
+start_builder.row(InlineKeyboardButton(text="🔑Мой ключ", callback_data="get_key"), InlineKeyboardButton(text="ℹ️ Инструкция", callback_data="instruction"))
+menu_builder = InlineKeyboardBuilder()
+menu_builder.row(InlineKeyboardButton(text="⬅️ Назад в меню", callback_data="back_to_menu"))
+
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     handlers=[logging.FileHandler("bot.log"), logging.StreamHandler()]
@@ -24,19 +27,33 @@ logging.basicConfig(level=logging.INFO,
 logger = logging.getLogger(__name__)
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Welcome to the VPN Bot! Use /getconfig to get your VPN configuration.", reply_markup=builder.as_markup())
+    await message.edit_text("Привет! Добро пожаловать в бот!", reply_markup=start_builder.as_markup())
 
 
 
-@dp.callback_query(F.data == "get_config")
-async def get_config(callback_query: CallbackQuery):
+@dp.callback_query(F.data == "get_key")
+async def get_key(callback_query: CallbackQuery):
     await callback_query.answer()
     try:
         link = vpn_service.get_user_config(str(callback_query.from_user.id))
-        await callback_query.message.answer(f"Your VPN configuration link: {link}")
+        await callback_query.message.edit_text(f"Your VPN configuration link: {link}", reply_markup=menu_builder.as_markup())
     except Exception as e:
         logger.error(f"Error occurred while fetching VPN config: {str(e)}")
         await callback_query.message.answer(f"Error: {str(e)}")
+
+
+@dp.callback_query(F.data == "instruction")
+async def show_instruction(callback_query: CallbackQuery):
+    await callback_query.answer()
+    await callback_query.message.edit_text("Here is how to use the VPN configuration.", reply_markup=menu_builder.as_markup())
+
+
+@dp.callback_query(F.data == "back_to_menu")
+async def back_to_menu(callback_query: CallbackQuery):
+    await callback_query.answer()
+    await callback_query.message.edit_text("Вы находитесь в главном меню.", reply_markup=start_builder.as_markup())
+
+
 
 if __name__ == '__main__':
     from asyncio import run
